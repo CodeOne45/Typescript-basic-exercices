280bedca86e85d61f355cb2ab194c57f27fc12be
CodeOne45 <malikaman1999@gmail.com>
2021-10-06 00:00:45 +0200

    Added exerces files

---
+++ Exercises/exo1.ts
@@ -0,0 +1,70 @@
+/*
+Source :  https://typescript-exercises.github.io/#exercise=5&file=%2Fnode_modules%2Ftype-assertions%2Findex.ts
+Welcome to:
+
+    ................................................................
+    .                                                              .
+    .     ####################    ####################      E      .
+    .     ####################    ####################      X      .
+    .             ####            ####                      E      .
+    .             ####            ####                      R      .
+    .             ####            ####################      C      .
+    .             ####                            ####      I      .
+    .             ####                            ####      S      .
+    .             ####            ####################      E      .
+    .             ####            ####################      S      .
+    .                                                              .
+    ................................................................
+
+    The goal: Let everyone play with many different TypeScript features
+    and get an overview of TypeScript capabilities and principles.
+
+    Things to cover:
+
+        1. Basic typing.
+        2. Refining types.
+        3. Union types.
+        4. Merged types.
+        5. Generics.
+        6. Type declarations.
+        7. Module augmentation.
+        8. Advanced type mapping.
+
+    Rules and principles:
+
+        1. Avoid using "any" type at all costs.
+        2. Difficulty quickly grows one exercise after another.
+        3. Enjoy.
+
+Exercise 1:
+
+    Given the data, define the interface "User" and use it accordingly.
+
+*/
+
+export type User = unknown;
+
+export const users: unknown[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    }
+];
+
+export function logPerson(user: unknown) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+
+console.log('Users:');
+users.forEach(logPerson);
+
+/* In case if you are stuck:
+help : https://www.typescriptlang.org/docs/handbook/interfaces.html#introduction
+*/
+
